e386a6c6246d62afd98324e78ac930561453991c
IliyaForsati <iliyaforsati2005@gmail.com>
2025-03-03 07:52:45 +0330

    add dbcontext and write model

---
+++ TinyURL.Domain/Models/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyURL.Domain.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string OriginalURL { get; set; }
+        public string ShortCutURL { get; set; }
+    }
+}
